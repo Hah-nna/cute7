93bb2dacf4a9a893670fb14731af55b7d0af91a1
doho <115533587+doho090@users.noreply.github.com>
2022-11-24 17:07:50 +0900

    .
    .
    
    Signed-off-by: doho <115533587+doho090@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,60 @@
+import { authService } from "./firebase.js";
+import {
+  createComment,
+  deletePoster,
+  updateComment,
+  deleteComment,
+  onEnterKey,
+  editComment,
+  cancelEditComment,
+} from "./pages/poster.js";
+import {
+  handleLocation,
+  goToProfile,
+  goToPost,
+  goToProfile_Edit,
+  goToMain,
+} from "./router.js";
+import { fil, onFileChange, changeProfile } from "./pages/profile_edit.js";
+
+
+import {toggle,setTimeout1}from "./pages/auth.js";
+
+
+
+// hash url 변경 시 처리
+window.addEventListener("hashchange", handleLocation);
+
+// 첫 랜딩 또는 새로고침 시 처리
+document.addEventListener("DOMContentLoaded", handleLocation);
+
+// 로그인 상태 모니터링
+authService.onAuthStateChanged((user) => {
+  // Firebase 연결되면 화면 표시
+  // user === authService.currentUser 와 같은 값
+  handleLocation();
+  if (user) {
+    // 로그인 상태인 경우
+  } else {
+    // 로그아웃 상태인 경우
+  }
+});
+
+// 전역 함수 리스트
+// window.route = route;
+window.deletePoster = deletePoster;
+window.createComment = createComment;
+window.editComment = editComment;
+window.cancelEditComment = cancelEditComment;
+window.updateComment = updateComment;
+window.deleteComment = deleteComment;
+window.onEnterKey = onEnterKey;
+window.fil = fil;
+window.goToProfile = goToProfile;
+window.goToPost = goToPost;
+window.onFileChange = onFileChange;
+window.changeProfile = changeProfile;
+window.goToProfile_Edit = goToProfile_Edit;
+window.goToMain = goToMain;
+window.toggle = toggle;
+window.setTimeout1 = setTimeout1;
