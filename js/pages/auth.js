96a3115e9fb78003ba7ff6f6281eabdb9ff31f20
doho <115533587+doho090@users.noreply.github.com>
2022-11-24 19:48:04 +0900

    Add files via upload
    Signed-off-by: doho <115533587+doho090@users.noreply.github.com>

--- js/pages/auth.js
+++ js/pages/auth.js
@@ -1,11 +1,17 @@
-toggle = () => {
+
+
+export const toggle = () => {
 	let container = document.getElementById('container')
 	container.classList.toggle('sign-in')
 	container.classList.toggle('sign-up')
 }
 
-setTimeout1(() => {
+
+export const addsign = () => {
+  console.log("addsign")
+  setTimeout(() => {
 	let container = document.getElementById('container')
 	container.classList.add('sign-in')
-}, )
 
+  }, 200);	
+}
