2d941ca6a2a6caae956044a45f7ccde748994d4f
nno3onn <nno3onn@naver.com>
2022-11-26 18:12:04 +0900

    fix, remove consoles

--- js/pages/cutemain.js
+++ js/pages/cutemain.js
@@ -69,24 +69,19 @@ export function fill() {
   //쉽게 보면 글검색창에 단어를 입력시 value에 넣어준다라고 생각하면됨
   // console.log(value);
   item = document.getElementById("main_columns").children;
-  console.log(item);
   // "profile_post_side"클래스, 즉 이미 있는 글내용과 닉네임이 있는 클래스에 포함된 모든 문자열을 item에 넣어준다.
-  console.log(item.length);
   for (i = 0; i < item.length; i++) {
     // item에는 item[0], item[1], item[2] 현재 이렇게 담겨져있다.
-
     title = item[i].getElementsByClassName("main_title");
     // 각 아이템 배열에서 클래스 contents, 즉 글내용 부분을 담는다.
     name = item[i].getElementsByClassName("main_name");
     // 각 아이템 배열에서 클래스 nick, 즉 닉네임 부분을 담는다
     // content = item[i].getDocs(collection(dbService, "post")); //여기에 content도 불러와야하는데 어떻게 불러오는지 일단 모르겠다.
-    console.log(title[0].innerHTML, name[0].innerHTML, name);
     if (
       title[0].innerHTML.indexOf(value) !== -1 ||
       name[0].innerHTML.indexOf(value) !== -1
       // || content[0].innerHTML.indexOf(value) > -1 //여기에 content도 불러와야하는데 어떻게 불러오는지 일단 모르겠다.
     ) {
-      console.log(name, title, item);
       // innerHTML은 HTML의 컨텐츠, 즉 내용에 접근할 수 있는 변수이고
       // indexOf()는 괄호안 값 문자열의 위치가 0,1,2,3,4... 인지 반환, 만약 없으면 -1을 반환한다.
 
